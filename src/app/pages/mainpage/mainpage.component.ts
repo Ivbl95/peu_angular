7efe99182cf222925f4fe268c079d775f79ee134
Иван Беляков <belyakov@jamm.ru>
2023-01-02 15:49:42 +0300

    fix (onoinit removed)

--- src/app/pages/mainpage/mainpage.component.ts
+++ src/app/pages/mainpage/mainpage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { python } from "../../../docs/python";
 import { js } from "../../../docs/js";
 import { angular } from "../../../docs/angular";
